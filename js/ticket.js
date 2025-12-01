5363619a3705ad7a57af172dbde131b31c977722
Nahu88 <meliannahu@gmail.com>
2025-12-01 11:30:39 -0300

    Minor fix ticket y boton admin

--- js/ticket.js
+++ js/ticket.js
@@ -1,16 +1,13 @@
 const ticketDiv = document.getElementById("ticket");
 const carrito = JSON.parse(localStorage.getItem("ticket")) || [];
-const cliente = localStorage.getItem("cliente");
+const clienteData = JSON.parse(localStorage.getItem("cliente") || "{}");
+const nombreCliente = clienteData.nombre || "Cliente";
 
 let total = 0;
-let html = `<p>Cliente: <strong>${cliente}</strong></p><ul>`;
+let html = `<p>Cliente: <strong>${nombreCliente}</strong></p><ul>`;
 
 carrito.forEach(p => {
-<<<<<<< HEAD
-  html += `<li>${p.titulo} x${p.cantidad} = $${p.precio * p.cantidad}</li>`;
-=======
   html += `<li>${p.titulo || p.nombre} x${p.cantidad} = $${p.precio * p.cantidad}</li>`;
->>>>>>> 4530bbf5a1910889b3d4d08309a5f7304cf57e0a
   total += p.precio * p.cantidad;
 });
 
